0fa54ae8f4282ade22d4c1761f9ec9dae06176f3
Abdul2Harris <harriscaspian9@gmail.com>
2025-07-04 16:48:04 +0530

    Updated Changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ import { HeroSection } from "../components/HeroSection"
 import { About } from "../components/About"
 import { Skills } from "../components/Skills"
 import { Projects } from "../components/Projects"
-import { Contact } from "../components/contact"
 import { Footer } from "../components/Footer"
+import { Contact } from "../components/Contact"
 
 
 export const Home = () => {
